c63f48925d9a17f45cb683c78348c3336960e29b
dhaveed <davidadegoke31@gmail.com>
2019-01-14 00:24:07 +0100

    price slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,45 @@
-$('.like-btn').on('click', function () {
-	$(this).toggleClass('is-active');
+$('.like-btn').on('click', function() {
+    $(this).toggleClass('is-active');
 });
-$('.minus-btn').on('click', function (e) {
-	e.preventDefault();
-	var $this = $(this);
-	var $input = $this.closest('div').find('input');
-	var value = parseInt($input.val());
+$('.minus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
 
-	if (value > 1) {
-		value -= 1;
-	}
-	else {
-		value = 0;
-	}
-	$input.val(value);
+    if (value > 1) {
+        value -= 1;
+    } else {
+        value = 0;
+    }
+    $input.val(value);
 });
-$('.plus-btn').on('click', function (e) {
-	e.preventDefault();
-	var $this = $(this);
-	var $input = $this.closest('div').find('input');
-	var value = parseInt($input.val());
+$('.plus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
 
-	if (value < 100) {
-		value += 1;
-	}
-else
-	{
-		value = 100;
-	}
-	$input.val(value);
+    if (value < 100) {
+        value += 1;
+    } else {
+        value = 100;
+    }
+    $input.val(value);
+});
+
+var nls = document.getElementById('priceSlider');
+noUiSlider.create(nls, {
+    connect: true,
+    behaviour: 'tap',
+    tooltips: [true, true],
+    start: [10000, 45000],
+    range: {
+        'min': [10000],
+        'max': [100000]
+    }
+});
+var nodes = [document.getElementById('lower'), document.getElementById('upper')];
+nonLinearSlider.noUiSlider.on('update', function (values, handle, unencoded, isTap, positions) {
+    nodes[handle].innerHTML = '&#8358;' + values[handle] + ', ' + positions[handle].toFixed(2) + '%';
 });
